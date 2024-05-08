1024292db5c52e579d2d8c4dfd2e7df71d930746
Radadiya Abhay <abhay592004@gmail.com>
2024-05-08 07:32:57 +0530

    Final_Submission

--- routes/index.js
+++ routes/index.js
@@ -20,46 +20,47 @@ import onlineorderRoutes from "./onlineorder.router.js"; //parth
 import ratingRoutes from "./rating.routes.js"; // Gautam
 import waitlistRoutes from "./waitlist.routes.js"; // Harshal
 import categoryRoutes from "./category.routes.js"; // Harshal
-// import customerRewardsRoutes from "./customerRewards.routes.js"; // dvij
 import userVoucherRoutes from "./userVoucher.routes.js"; // dvij
 import voucherRoutes from "./voucher.routes.js"; // dvij
 import rewardRoutes from "./reward.routes.js"; // dvij
 import orderTrackingRouter from "./ordertracking.js"; // udit
 
 
+
+// if you Run Call This Api so First Of All You Genrate Admin And Add Staff And After Add Role And save role Permission After Call Api
+// Second Option is Middleware Remove for Call Api
+
 let routes = express()
 
 routes.get('/', (req, res) => {
     res.json({ message: 'Welcome to the API' })
 });
-routes.use('/booking', bookingRoutes)
-routes.use('/customer', customerRoutes)
-routes.use('/table',  tableRoutes)
-routes.use('/staffrole',  staffRoleRoutes)
-routes.use('/ingredients', ingredientsRoutes)
-routes.use('/menu', menuRoutes)
-routes.use('/menuingredients', menuIngredientsRoutes)
-routes.use('/order', orderRoutes)
-routes.use('/staff', staffRoutes)
+routes.use('/booking',authLogin, bookingRoutes)
+routes.use('/customer',staffAuthLogin , roleService, customerRoutes)
+routes.use('/table',staffAuthLogin , roleService,  tableRoutes)
+routes.use('/staffrole',staffAuthLogin , roleService,  staffRoleRoutes)
+routes.use('/ingredients',staffAuthLogin , roleService, ingredientsRoutes)
+routes.use('/menu',staffAuthLogin , roleService, menuRoutes)
+routes.use('/menuingredients',staffAuthLogin , roleService, menuIngredientsRoutes)
+routes.use('/order',authLogin, orderRoutes)
+routes.use('/staff',staffAuthLogin , roleService, staffRoutes)
 
-routes.use('/ordermenu', orderMenuRoutes)
-routes.use('/onlineorder', onlineorderRoutes)
-routes.use('/staff', staffRoutes)
+routes.use('/ordermenu',staffAuthLogin , roleService, orderMenuRoutes)
+routes.use('/onlineorder',staffAuthLogin , roleService, onlineorderRoutes)
 
-routes.use('/menuitem', menuItemRoutes)
-routes.use('/offer',offerRoutes)
+routes.use('/menuitem',staffAuthLogin , roleService, menuItemRoutes)
+routes.use('/offer',staffAuthLogin , roleService,offerRoutes)
 routes.use('/auth', userRoutes)
-routes.use('/menutime', menuTimeRoutes);
-routes.use('/rating', ratingRoutes)
-routes.use('/category', categoryRoutes)
-routes.use('/waitlist', waitlistRoutes)
+routes.use('/menutime',staffAuthLogin , roleService, menuTimeRoutes);
+routes.use('/rating',authLogin, ratingRoutes)
+routes.use('/category',staffAuthLogin , roleService, categoryRoutes)
+routes.use('/waitlist',staffAuthLogin , roleService, waitlistRoutes)
 
-routes.use('/ordertracking', orderTrackingRouter)
+routes.use('/ordertracking',authLogin, orderTrackingRouter)
 
-routes.use('/voucher', voucherRoutes)
-routes.use('/uservoucher', userVoucherRoutes)
-// routes.use('/customerRewards', customerRewardsRoutes)
-routes.use('/reward', rewardRoutes)
+routes.use('/voucher',authLogin, voucherRoutes)
+routes.use('/uservoucher',authLogin, userVoucherRoutes)
+routes.use('/reward',authLogin, rewardRoutes)
 
 
 export default routes
